3f138c42f0bea42a3774b52d2c4319ea398bb7c1
David Gregori Rodriguez Calle <www.str_hpl@hotmail.com>
2025-09-12 16:13:35 -0400

    configuraciones para playwright

---
+++ DavidRodriguez/README.md
@@ -0,0 +1,16 @@
+# Primer Framework Playwright/Java
+
+# Como usar
+
+Inicializa el proyecto Node.js con valores por defecto
+```
+npm init -y
+```
+Instala Playwright Test como dependencia de desarrollo
+```
+npm install -D @playwright/test
+```
+Descarga los navegadores soportados por Playwright
+```
+npx playwright install
+```
---
+++ DavidRodriguez/playwright.config.js
@@ -0,0 +1,22 @@
+// Playwright configuration
+
+/** @type {import{'@playwright/test'}.PlaywrightTestConfig}*/
+
+const config = {
+    use: {
+        screenshot: "only-on-failure",
+        trace: "retain-on-failure",
+    },
+    projects: [
+        //{name: "chromium", use: {browserName: "chromium"}},
+        {name: "Firefox", use: {browserName: "firefox"}},
+        //{name: "webkit", use: {browserName: "webkit"}},
+    ],
+    testDir: 'tests',
+    timeout: 30000,
+    expect: {
+        timeout: 5000,
+    }, 
+};
+
+module.exports = config;
