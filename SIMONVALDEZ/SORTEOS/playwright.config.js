6db4b19c62e39e0689caf2cae6cdaa676232a5d8
Simon Valdez <simovaldez414@gmail.com>
2025-09-16 18:25:58 -0400

    Simon Valdez

--- SIMONVALDEZ/SORTEOS/playwright.config.js
+++ SIMONVALDEZ/SORTEOS/playwright.config.js
@@ -1,10 +1,6 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
   import dotenv from 'dotenv';
   import path from 'path';
   dotenv.config({ path: path.resolve(__dirname, '.env') });
@@ -30,16 +26,12 @@ export default defineConfig({
         timeout: 5000
     },
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     baseURL: process.env.BASE_URL || 'https://app-sorteos.com/es',
     headless: process.env.HEADLESS !== 'false', // Controlar headless con variable de entorno
     trace: 'on-first-retry',
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
