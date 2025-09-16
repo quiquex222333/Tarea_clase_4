1fd587c30c5ad675b320de5733e6f369a0400737
Gabriela Cuellar <cuellar.caero.gabriela@gmail.com>
2025-09-16 18:57:00 -0400

    Test pagina 2

--- GabrielaCuellar/utils/config.py
+++ GabrielaCuellar/utils/config.py
@@ -1,4 +1,5 @@
 import os
 
 BASE_URL_A = os.getenv('BASE_URL', 'https://parabank.parasoft.com/parabank/index.htm')
+BASE_URL_B = os.getenv('BASE_URL', 'https://demoqa.com/books')
 HEADLESS = os.getenv('HEADLESS', 'false').lower() == 'true'
