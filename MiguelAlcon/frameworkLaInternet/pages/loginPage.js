e1fbab852db9ab6f492ec77891e75fa4be62e886
miguelangel8888 <mkalyu81@gmail.com>
2025-09-16 18:49:32 -0400

    agregando todos los archivos del framework

---
+++ MiguelAlcon/frameworkLaInternet/pages/loginPage.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export class LoginPage {
+  /**
+   * @param {import('@playwright/test').Page} page
+   */
+  constructor(page) {
+    this.page = page;
+    this.usernameInput = page.locator('#username');
+    this.passwordInput = page.locator('#password');
+    this.loginButton = page.locator('.radius');
+  }
+
+  async goto() {
+    await this.page.goto(`${process.env.BASE_URL}/login`);
+  }
+
+  async login(username, password) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+}
