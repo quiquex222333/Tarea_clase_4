4bf61aa50424632b362a3dae890a8b54a4a42243
Gabriela Cuellar <cuellar.caero.gabriela@gmail.com>
2025-09-16 18:22:54 -0400

    Testeo de login credenciales validas y vacias

--- GabrielaCuellar/utils/config.py
+++ GabrielaCuellar/utils/config.py
@@ -1,4 +1,4 @@
 import os
 
 BASE_URL_A = os.getenv('BASE_URL', 'https://parabank.parasoft.com/parabank/index.htm')
-HEADLESS = os.getenv('HEADLESS', 'true').lower() == 'true'
+HEADLESS = os.getenv('HEADLESS', 'false').lower() == 'true'
