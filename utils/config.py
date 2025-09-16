7412f51fee87b62a907c0122dd8a6b8520c84c32
Espinoza Noe <nj.espinoza714@gmail.com>
2025-09-16 12:31:42 -0300

    configuracion  test

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,7 @@
 # creacion y configuracion de base URL  Y PASSWORD LOGIN
 import os
 
-BASE_URL = os.getenv('BASE_URL', 'htps://www.mercadolibre.com') #  direcion de URla
+BASE_URL = os.getenv('BASE_URL', 'htps://https://www.saucedemo.com/') #  direcion de URl
 HEADLESS = os.getenv('HEADLESS', 'true').lower()== 'true'
 #USERNAME = os.getenv('USERNAME', 'RABBIT714') #NOMBRE DE USUARIO
 #PASSWORD = os.getenv('PASSWORD','12345678') 
